a662d4f8afb6dd9db4cd9f88b3fb68458bf5ede9
server <root@server>
2020-02-24 14:08:07 +0100

    Now filtering on weerstation id

--- buienradarMQTT.php
+++ buienradarMQTT.php
@@ -17,34 +17,60 @@ if (($tmp = $iniarray["buienradarMQTT"]["mqttport"]) != "") $tcpport = $tmp;
 if (($tmp = $iniarray["buienradarMQTT"]["mqttusername"]) != "") $username = $tmp;
 if (($tmp = $iniarray["buienradarMQTT"]["mqttpassword"]) != "") $password = $tmp;
 
+$statustopic = $mqttTopicPrefix."/status";
+
+$starttime = time();
+
+$will = array();
+$will["topic"] = $statustopic;
+$will["content"] = "offline";
+$will["qos"] = 1;
+$will["retain"] = 1;
 
 $mqtt = new phpMQTT($server, $port, $client_id);
-$mqtt->connect(true, NULL, $username, $password);
+$mqtt->connect(true, $will, $username, $password);
+$mqtt->publish($statustopic, "online", 1, 1);
 
 $timeouttimer = 0;
-
+$firstrun = true;
 while (1)
 {
         if ($timeouttimer == 0)
         {
-                        // For now we use synchone fetching of buienradar because they changed from http to https :-(
-                        echo ("Fetching data from  buienradar server...\n");
-                        //$buienradarsocket = socketconnect('xml.buienradar.nl', 443);
-                       $url = "https://xml.buienradar.nl";
-                       $array = [];
-                       $simpleXml = simplexml_load_file($url);
-                       if ($simpleXml)
-                       {
-                               	simplexml_to_array($simpleXml, $array, "home/buienradar");
-                        }
+                $timeouttimer = 600;
+                // For now we use synchone fetching of buienradar because they changed from http to https :-(
+                echo ("Fetching data from  buienradar server...\n");
+                //$buienradarsocket = socketconnect('xml.buienradar.nl', 443);
+                $url = "https://xml.buienradar.nl";
+                $array = [];
+                $mqtt->publishwhenchanged($statustopic, "fetching", 1, 1);
+                $simpleXml = simplexml_load_file($url);
+                if ($simpleXml)
+                {
+                        simplexml_to_array($simpleXml, $array, "home/buienradar");
+                        $mqtt->publishwhenchanged($statustopic, "ready", 1, 1);
+                }
+                else 
+                {
+                        $mqtt->publishwhenchanged($statustopic, "commerror", 1, 1);
+                        $timeouttimer = 60;
+                }
         }
         
-        if ($timeouttimer == 600) $timeouttimer = 0;
-        else $timeouttimer++;
-        
-        $mqtt->proc();
-        sleep(1);
+        $timeouttimer--;
+//        echo ($timeouttimer . "\n");
 
+        $uptime = time() - $starttime;
+        if (($uptime % 60 == 0) || $firstrun)
+        {
+                $s = time()-$starttime;
+                $uptimestr = sprintf('%d:%02d:%02d:%02d', $s/86400, $s/3600%24, $s/60%60, $s%60);
+                $mqtt->publishwhenchanged("home/buienradar/system/uptime", $uptimestr, 1, 1);
+        }
+
+        $mqtt->proc();
+        usleep(10000);
+        $firstrun = false;
 }        
         
  function simplexml_to_array ($xml, &$array, $origpath, $next = 0)
@@ -59,7 +85,14 @@ while (1)
         foreach ($xml->children() as $child) {
         if ($next) $path = $origpath."/".$child->getName();
         else $path = $origpath;
-        if ($child->getName() == "weerstation") $path.="/".$child->attributes()["id"];
+        $skipitem = false;
+        if ($child->getName() == "weerstation") 
+        {
+                $path.="/".$child->attributes()["id"];
+                if ($child->attributes()["id"] != "6370") $skipitem = true;
+        }
+        if (!$skipitem)
+        {
                 $nrofsamechilds = 0;
                 foreach ($xml->children() as $searchchild)
                 {
@@ -74,17 +107,20 @@ while (1)
                         simplexml_to_array($child, $array[$xml->getName()], $path,1);
                 }
         }
+        }
         
 
         // Node attributes
         foreach ($xml->attributes() as $key => $att) {
                 $array[$xml->getName()]['@attributes'][$key] = (string) $att;
+//                echo ($origpath."/@/".$key."=".(string) $att."\n");
+                $mqtt->publishwhenchanged($origpath."/@/".$key, (string) $att, 0, 1);
         }
 
         // Node with value
         if (trim((string) $xml) != '') {
                 $array[$xml->getName()][] = (string) $xml;
-                echo ($origpath."=".(string) $xml."\n");
+//                echo ($origpath."=".(string) $xml."\n");
                 $mqtt->publishwhenchanged($origpath, (string) $xml, 0, 1);
         }
 
